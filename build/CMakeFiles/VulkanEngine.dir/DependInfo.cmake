72938bfafd0042cd6cf61cb2ae005b4ec1406d86
Lounol72 <subtil.louis2005@gmail.com>
2026-03-19 23:03:11 +0100

    Transition Makefile -> Cmake

---
+++ build/CMakeFiles/VulkanEngine.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/lounol72/Projets/Cpp/vulkanEngine/src/app.cpp" "CMakeFiles/VulkanEngine.dir/src/app.cpp.o" "gcc" "CMakeFiles/VulkanEngine.dir/src/app.cpp.o.d"
+  "/home/lounol72/Projets/Cpp/vulkanEngine/src/buffer.cpp" "CMakeFiles/VulkanEngine.dir/src/buffer.cpp.o" "gcc" "CMakeFiles/VulkanEngine.dir/src/buffer.cpp.o.d"
+  "/home/lounol72/Projets/Cpp/vulkanEngine/src/camera.cpp" "CMakeFiles/VulkanEngine.dir/src/camera.cpp.o" "gcc" "CMakeFiles/VulkanEngine.dir/src/camera.cpp.o.d"
+  "/home/lounol72/Projets/Cpp/vulkanEngine/src/descriptors.cpp" "CMakeFiles/VulkanEngine.dir/src/descriptors.cpp.o" "gcc" "CMakeFiles/VulkanEngine.dir/src/descriptors.cpp.o.d"
+  "/home/lounol72/Projets/Cpp/vulkanEngine/src/device.cpp" "CMakeFiles/VulkanEngine.dir/src/device.cpp.o" "gcc" "CMakeFiles/VulkanEngine.dir/src/device.cpp.o.d"
+  "/home/lounol72/Projets/Cpp/vulkanEngine/src/game_object.cpp" "CMakeFiles/VulkanEngine.dir/src/game_object.cpp.o" "gcc" "CMakeFiles/VulkanEngine.dir/src/game_object.cpp.o.d"
+  "/home/lounol72/Projets/Cpp/vulkanEngine/src/keyboard_movement_controller.cpp" "CMakeFiles/VulkanEngine.dir/src/keyboard_movement_controller.cpp.o" "gcc" "CMakeFiles/VulkanEngine.dir/src/keyboard_movement_controller.cpp.o.d"
+  "/home/lounol72/Projets/Cpp/vulkanEngine/src/model.cpp" "CMakeFiles/VulkanEngine.dir/src/model.cpp.o" "gcc" "CMakeFiles/VulkanEngine.dir/src/model.cpp.o.d"
+  "/home/lounol72/Projets/Cpp/vulkanEngine/src/pipeline.cpp" "CMakeFiles/VulkanEngine.dir/src/pipeline.cpp.o" "gcc" "CMakeFiles/VulkanEngine.dir/src/pipeline.cpp.o.d"
+  "/home/lounol72/Projets/Cpp/vulkanEngine/src/renderer.cpp" "CMakeFiles/VulkanEngine.dir/src/renderer.cpp.o" "gcc" "CMakeFiles/VulkanEngine.dir/src/renderer.cpp.o.d"
+  "/home/lounol72/Projets/Cpp/vulkanEngine/src/simple_render_system.cpp" "CMakeFiles/VulkanEngine.dir/src/simple_render_system.cpp.o" "gcc" "CMakeFiles/VulkanEngine.dir/src/simple_render_system.cpp.o.d"
+  "/home/lounol72/Projets/Cpp/vulkanEngine/src/swap_chain.cpp" "CMakeFiles/VulkanEngine.dir/src/swap_chain.cpp.o" "gcc" "CMakeFiles/VulkanEngine.dir/src/swap_chain.cpp.o.d"
+  "/home/lounol72/Projets/Cpp/vulkanEngine/src/window.cpp" "CMakeFiles/VulkanEngine.dir/src/window.cpp.o" "gcc" "CMakeFiles/VulkanEngine.dir/src/window.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/VulkanEngineShaders.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/lounol72/Projets/Cpp/vulkanEngine/shaders/shader.frag.spv"
+  "/home/lounol72/Projets/Cpp/vulkanEngine/shaders/shader.vert.spv"
+  "CMakeFiles/VulkanEngineShaders"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/VulkanEngineShaders.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
